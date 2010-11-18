2a269936ffb95bff06c4c1f4b65e14ddd950b51c
Zeev Tarantov <zeev.tarantov@gmail.com>
2010-11-18 13:45:33 +0200

    implement less branchy do_before_step

--- jit_asm.c
+++ jit_asm.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 
 /*
 You must compile this file with -fomit-frame-pointer
@@ -41,53 +42,63 @@ void do_light(int y, int x, int dir, void *map)
   asm(
     "leaq  (%rsi,%rdi,8), %r8\n"
     "movb  (%rcx,%r8), %al\n"
-    "mov   %al, %bl\n"
+    "movb  %al, %bl\n"
     "shr   %al\n"
-    "xor   %al, %bl\n"
-    "and   $0xbf, %bl\n"
-    "and   $0x40, %al\n"
-    "or    %bl, %al\n"
-    "mov   %al, (%rcx,%r8)\n"
+    "xorb  %al, %bl\n"
+    "andb  $0xbf, %bl\n"
+    "andb  $0x40, %al\n"
+    "orb   %bl, %al\n"
+    "movb  %al, (%rcx,%r8)\n"
   );
 }
 
 static void __attribute__((noinline)) do_before_step(int y, int x, int dir, void *map)
 {
   asm(
-  "leaq (%rsi,%rdi,8), %rax\n"
-  "movb (%rcx,%rax,1), %al\n"
-  "andb $63,  %al\n"
-  "movq %rdi, %r8\n"
-  "movq %rsi, %r9\n"
-  "cmpb $0,   %dl\n"
-  "je   case_n\n"
-  "cmpb $1,   %dl\n"
-  "je   case_e\n"
-  "cmpb $2,   %dl\n"
-  "je   case_s\n"
-  "case_w:\n"
-  "cmpb $0,   %sil\n"
-  "jbe  step_end\n"
-  "decb %r9b\n"
-  "jmp  step_end\n"
-  "case_s:\n"
-  "cmpb $0,   %dil\n"
-  "jbe  step_end\n"
-  "decb %r8b\n"
-  "jmp  step_end\n"
-  "case_e:\n"
-  "cmpb $6,   %sil\n"
-  "ja   step_end\n"
-  "incb %r9b\n"
-  "jmp  step_end\n"
-  "case_n:\n"
-  "cmpb $3,   %dil\n"
-  "ja   step_end\n"
-  "incb %r8b\n"
-  "step_end:\n"
-  "leaq (%r9,%r8,8), %rbx\n"
-  "movb (%rcx,%rbx,1), %bl\n"
-  "andb $63,  %bl\n"
+    "movq %rdi, %r8\n"
+    "movq %rsi, %r9\n"
+    "cmpb $0,   %dl\n"
+    "je   case_n\n"
+    "cmpb $1,   %dl\n"
+    "je   case_e\n"
+    "cmpb $2,   %dl\n"
+    "je   case_s\n"
+    "case_w:\n"
+    "decb %r9b\n"
+    "movb %r9b, %al\n"
+    "sarb $7,   %al\n"
+    "notb %al\n"
+    "andb %al,  %r9b\n"
+    "jmp  step_end\n"
+    "case_s:\n"
+    "decb %r8b\n"
+    "movb %r8b, %al\n"
+    "sarb $7,   %al\n"
+    "notb %al\n"
+    "andb %al,  %r8b\n"
+    "jmp  step_end\n"
+    "case_e:\n"
+    "incb %r9b\n"
+    "subb $7,   %r9b\n"/* max_x_coord */
+    "movb %r9b, %al\n"
+    "sarb $7,   %al\n" /* CHAR_BITS - 1 */
+    "andb %al,  %r9b\n"
+    "addb $7,   %r9b\n"/* max_x_coord */
+    "jmp  step_end\n"
+    "case_n:\n"
+    "incb %r8b\n"
+    "subb $4,   %r8b\n"/* max_y_coord */
+    "movb %r8b, %al\n"
+    "sarb $7,   %al\n" /* CHAR_BITS - 1 */
+    "andb %al,  %r8b\n"
+    "addb $4,   %r8b\n"/* max_y_coord */
+    "step_end:\n"
+    "leaq (%rsi,%rdi,8), %rax\n"
+    "leaq (%r9, %r8, 8), %rbx\n"
+    "movb (%rcx,%rax,1), %al\n"
+    "movb (%rcx,%rbx,1), %bl\n"
+    "andb $63,  %al\n"
+    "andb $63,  %bl\n"
   );
 }
 
